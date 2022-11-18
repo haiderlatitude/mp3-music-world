7d62c244dccf46f775bed0f04f7e01cd81ce2947
haiderlatitude <haiderkhalid602@gmail.com>
2022-11-19 01:53:02 +0500

    Background image added in login page

--- resources/views/dashboard/head.blade.php
+++ resources/views/dashboard/head.blade.php
@@ -2,7 +2,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <link rel="shortcut icon" type="image/x-icon" href="#">
-    <title>MP3 Music - {{ $title }}</title>
+    <title>MP3 Music World - {{ $title }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
