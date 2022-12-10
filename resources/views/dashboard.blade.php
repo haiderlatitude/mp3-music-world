15a64bfb0e8c615d8dd49a13c84452f2106df552
haiderlatitude <haiderkhalid602@gmail.com>
2022-12-10 21:05:11 +0500

    home page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,10 @@
 @extends('dashboard.app', ['title' => 'Dashboard'])
 
 @section('body')
-    <h1 class='text-center'>Test Page</h1>
+    <h1 class='text-center'>
+        <ul>A search bar for searching songs...</ul>
+        <ul>Songs list after a search has been made...</ul>
+        <ul>An MP3 player at the bottom for playing the songs...</ul>
+    </h1>
 @endsection
 
